15a2a53117b097781ac81cd26107e85fdb3a8717
Sydney <sydneyhoweacademy@gmail.com>
2022-06-22 11:59:12 -0400

    add color function

--- getHome.py
+++ getHome.py
@@ -4,6 +4,14 @@ timer = time.time()
 right = True
 #You can define helper functions here, make sure to but them *above* the main function
 target_color = (255, 0 ,0)
+
+def myFunction(color: tuple):
+    if color[0] > color[1] and color[0] > color[2]:
+            return "red"
+    elif color[1] > color[0] and color[1] > color[2]:
+            return "green"
+    elif color[2] > color[0] and color[2] > color[1]:
+            return "blue"
 def main(robot: libhousy.robot):
     global right, timer
     #Here is where your recurring code will go
